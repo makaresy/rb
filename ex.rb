a5d8666f0cb3d225cc950b67cb216f45a2f3ac30
maeasy <makaresy@gmail.com>
2020-09-16 16:04:54 +0300

    Add

--- ex.rb
+++ ex.rb
@@ -1,3 +1,11 @@
-puts "hello world"
-puts "commit 2"
-pust "commit 3"
+module M
+def self.extended(obj)
+puts "Module #{self} is being used by #{obj}."
+end
+def an_inst_method
+puts "This module supplies this instance method."
+end
+end
+my_object = Object.new
+my_object.extend(M)
+my_object.an_inst_method
